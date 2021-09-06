67e4fbb20e6da73b11228a84bb75784836b01d02
darioB <dariovictorbarboso@hotmail.com>
2021-09-06 12:37:03 -0300

    Practica 07

---
+++ Practica07/funciones.js
@@ -0,0 +1,10 @@
+
+//Se puede llamar antes de declarar la función
+miFuncion(10,5);
+
+//Declaración de una función
+function miFuncion(a, b){//bloque de una función
+    console.log("Suma: " + ( a + b ) );
+}
+//Llamada a función
+miFuncion(15,5);
