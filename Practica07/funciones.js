34cf0676d9665cdf319bc88e945c9c021d044d68
darioB <dariovictorbarboso@hotmail.com>
2021-09-21 12:53:14 -0300

    arguments

--- Practica07/funciones.js
+++ Practica07/funciones.js
@@ -68,10 +68,55 @@ console.log(resultado);
  *cuando llamamos a la funcion: suma(5,3); sus argumentos son 5 y 3
  */
 
+let sumarV2 = function( a, b){
+    //Propiedad arguments es un arreglo/array
+    //nos retorna el primer valor de los argumentos
+   console.log(arguments[0]);
+   console.log(arguments[1]);    
+   return a+b;
+} 
+
+console.log( sumarV2(5,7));
+
+// Podemos no especificar todos los argunmentos
+
+console.log( sumarV2(5));
+//podemos dar valores por defecto
+
+let sumarV3 = function( a=5, b=7){
+   console.log(arguments[0]);
+   console.log(arguments[1]);    
+   //esta posicion no esta definida no posee parametros
+   console.log(arguments[2]);    
+   
+   //return a+b;
+   return a+b+arguments[2];
+} 
+//console.log( sumarV3() );
+
+//Pero si mandamos valores a la funcion se sobre escriben los valores por default
+console.log( sumarV3(4,9) );
+
+//Aun que no posea la funcion un parametro c aun asi puede obtenerse en arguments
+console.log( sumarV3(4,9,8) );
 
 
+/**
+ *  * Utilizamos el concepto de Hosting
+ *  * Es decir que podemos usar la funcion antes de declararla 
+ */
+// Se puede observar que da lo mismo si imprimo antes o despues la variable resultado
+resultado = sumarTodo( 5, 4, 13, 10, 9 );//se le puede enviar cualquier cantidad de argumentos
+console.log(resultado);
+function sumarTodo (){
+    let suma = 0;
+    for (let i = 0; i < arguments.length; i++) {
+         suma += arguments[i];   
+    }
+    return suma;
+}
 
-
+console.log(resultado);
 
 
 
